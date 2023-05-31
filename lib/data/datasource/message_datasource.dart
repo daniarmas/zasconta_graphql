bf20b18a1b700ec99934f4dae4091316cd9abe86
Daniel Armas <daniel.armas9706@gmail.com>
2023-05-31 17:38:43 -0400

    Creating the MessageDatasourceImpl class and fixing freezed generated code errors.

--- lib/data/datasource/message_datasource.dart
+++ lib/data/datasource/message_datasource.dart
@@ -1,5 +1,23 @@
 abstract class MessageDatasource {
   Future<void> connect();
   Future<void> close();
-  Stream<MessageModel> get streamMessage;
+  // Stream<MessageModel> get streamMessage;
+}
+
+class MessageDatasourceImpl implements MessageDatasource {
+  @override
+  Future<void> close() {
+    // TODO: implement close
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> connect() {
+    // TODO: implement connect
+    throw UnimplementedError();
+  }
+
+  // @override
+  // // TODO: implement streamMessage
+  // Stream get streamMessage => Stream.empty();
 }
--- lib/domain/models/message_model.dart
+++ lib/domain/models/message_model.dart
@@ -1,6 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'message_model.freezed.dart';
+part 'message_model.g.dart';
 
 @freezed
 class MessageModel with _$MessageModel {
